4bcd99d81481c3dfc8ed77fb1ab937110282ee06
dsaf fdsa <amiryusfsdupov.sdf070@gmfsadail.com>
2025-01-13 15:33:28 +0500

    sss

--- js/functions.js
+++ js/functions.js
@@ -2,10 +2,11 @@ document.addEventListener("DOMContentLoaded", function () {
     const myDiv = document.getElementById('mobile-menu');
     const toggleBtn = document.getElementById('width_changer');
     const menuBtn = document.querySelector('.main_menus');
-
+    const claudeText = document.querySelector('.clod_text')
     toggleBtn.addEventListener('click', function () {
         if (myDiv.classList.contains('menu_bottom')) {
             myDiv.classList.toggle('new-width');
+            claudeText.classList.toggle('clod_opacity')
         } 
     });
 });
